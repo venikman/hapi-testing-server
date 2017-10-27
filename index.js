b50d861863d813260dade80dce6bc435148b3245
Stan <nedbailov375426@gmail.com>
2017-10-26 23:49:35 -0400

    Clean up index.js
    ```
    throw new Error('package.json needs keywords.');
        throw new Error('README.md needs updating.');
        throw new Error('No code written yet.');
    ```
    Let’s leave that TODO list.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 'use strict';
 
-module.exports = () => {
-    throw new Error('package.json needs keywords.');
-    throw new Error('README.md needs updating.');
-    throw new Error('No code written yet.');
-};
+
