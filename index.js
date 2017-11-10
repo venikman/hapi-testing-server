b17a938b5d8e3bebf98b15595d97b69eaeaf806e
Stan <nedbailov375426@gmail.com>
2017-11-10 16:39:30 -0500

    Create simple server on v17.

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 'use strict';
 
 const Hapi = require('hapi');
-const Good = require('good');
-const doorkeeper = require('hapi-doorkeeper');
-const cookie = require('hapi-auth-cookie');
-const bell = require('bell');
 
-const server = new Hapi.Server();
-server.connection({
+const server = Hapi.Server({
     host : 'localhost',
     port : 3000
 });
@@ -15,58 +10,22 @@ server.connection({
 server.route({
     method  : 'GET',
     path    : '/{name}',
-    handler : (request, reply) => {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    options : {
+        handler : (request, h) => {
+            return 'Hello, ' + encodeURIComponent(request.params.name) + '!';
+         }
     }
 });
 
-server.register([{
-    register : Good,
-    options  : {
-        reporters : {
-            console : [{
-                module : 'good-squeeze',
-                name   : 'Squeeze',
-                args   : [{
-                    log      : '*',
-                    response : '*'
-                }]
-            },
-            {
-                module : 'good-console'
-            },
-                'stdout']
-        }
-    }
-},
-    bell,
-    cookie,
-{
-    register : doorkeeper,
-    options  : {
-        sessionSecretKey : 'venikmanhandballfewmoresimbolstogetenought',
-        auth0Domain      : 'nedbailov.auth0.com',
-        auth0PublicKey   : '4FvZeLbOnzu2qfxqZ9SLRAnbVLo53CSV',
-        auth0SecretKey   : 'Gu9saPRDoLE5rFG8c52JKdR5gY5clZVbZQCJjfC1ZImI7dMVnA7pPvICUP4DWW2w'
-    }
-}]
-);
-
 server.route({
     method : 'GET',
-    path   : '/home',
-    config : {
-        auth : {
-            strategy : 'session',
-            mode     : 'required'
+    path   : '/',
+    options : {
+        handler(request, h) {
+            return 'hi';
         }
-    },
-    handler(request, reply) {
-        reply('hi');
     }
 });
 
 server.start()
-.then(() => {
-    console.log(server.info.uri);
-});
+    .then(console.log(server.info.uri));
