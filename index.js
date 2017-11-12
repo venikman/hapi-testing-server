f2e567bbc25396350c0f80331c8816c3b0fc11a2
Stan <nedbailov375426@gmail.com>
2017-11-12 02:54:32 -0500

    Add vision, inert and handelbards plugins.
    Migration is going. Add 3 more plugins. Now I can do handlebars. Plus decided to use css framework. Bulma with font-awesome. Don’t want spend too much time for styling. And want to give a shot.

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
 'use strict';
 
+const path = require('path');
 const Hapi = require('hapi');
+const Vision = require('vision');
+const Inert = require('inert');
+const handlebars = require('handlebars');
 
 const server = Hapi.Server({
     host : 'localhost',
-    port : 3000
-});
-
-server.route({
-    method  : 'GET',
-    path    : '/{name}',
-    options : {
-        handler : (request, h) => {
-            return 'Hello, ' + encodeURIComponent(request.params.name) + '!';
-         }
-    }
-});
-
-server.route({
-    method : 'GET',
-    path   : '/',
-    options : {
-        handler(request, h) {
-            return 'hi';
+    port : 3001,
+    routes : {
+        files : {
+            relativeTo : path.join(__dirname, 'lib', 'static')
         }
     }
 });
 
-server.start()
-    .then(console.log(server.info.uri));
+server.register([
+    {
+        plugin : Vision
+    },
+    {
+        plugin : Inert
+    }
+]).then (() => {
+        // Template rendering configuration using "vision" plugin.
+        server.views({
+            engines : {
+                html : handlebars
+            },
+            relativeTo   : path.join(__dirname, 'lib', 'view'),
+            // Directories for views, helpers, partials, and layouts.
+            path         : '.',
+            helpersPath  : 'helper',
+            partialsPath : 'partial',
+            layoutPath   : 'layout',
+            // Name of the default layout file. Can be overriden in routes.
+            layout       : 'default-layout'
+        })
+
+        server.route([
+            require('./lib/route/static'),
+            require('./lib/route/root')
+        ])
+
+        server.start()
+        .then(console.log(server.info.uri))
+    })
+    .catch(err => {
+        throw err
+      })
--- test.js
+++ test.js
@@ -1,5 +1,11 @@
 import test from 'ava';
 
-test((t) => {
-    t.deepEqual([1, 2], [1, 2]);
+test('foo', (t) => {
+    t.pass();
+});
+
+test('bar', async (t) => {
+    const bar = Promise.resolve('bar');
+
+    t.is(await bar, 'bar');
 });
