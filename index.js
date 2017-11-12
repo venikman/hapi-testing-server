03dfdbd36ea75a86af9d37bdb69edaf0b61dd6c9
Stan <nedbailov375426@gmail.com>
2017-11-12 03:30:26 -0500

    Fix errors.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const path = require('path');
-const Hapi = require('hapi');
+const hapi = require('hapi');
 const Vision = require('vision');
 const Inert = require('inert');
 const handlebars = require('handlebars');
 
-const server = Hapi.Server({
-    host : 'localhost',
-    port : 3001,
+const server = hapi.server({
+    host   : 'localhost',
+    port   : 3001,
     routes : {
         files : {
             relativeTo : path.join(__dirname, 'lib', 'static')
@@ -23,30 +23,32 @@ server.register([
     {
         plugin : Inert
     }
-]).then (() => {
+]).then(() => {
         // Template rendering configuration using "vision" plugin.
-        server.views({
-            engines : {
-                html : handlebars
-            },
-            relativeTo   : path.join(__dirname, 'lib', 'view'),
+    server.views({
+        engines : {
+            html : handlebars
+        },
+        relativeTo   : path.join(__dirname, 'lib', 'view'),
             // Directories for views, helpers, partials, and layouts.
-            path         : '.',
-            helpersPath  : 'helper',
-            partialsPath : 'partial',
-            layoutPath   : 'layout',
+        path         : '.',
+        helpersPath  : 'helper',
+        partialsPath : 'partials',
+        layoutPath   : 'layout',
             // Name of the default layout file. Can be overriden in routes.
-            layout       : 'default-layout'
-        })
+        layout       : 'default-layout'
+    });
 
-        server.route([
-            require('./lib/route/static'),
-            require('./lib/route/root')
-        ])
+    /* eslint-disable global-require */
+    server.route([
+        require('./lib/route/static'),
+        require('./lib/route/root')
+    ]);
+    /* eslint-enable global-require */
 
-        server.start()
-        .then(console.log(server.info.uri))
-    })
-    .catch(err => {
-        throw err
-      })
+    server.start()
+        .then(console.log(server.info.uri));
+})
+    .catch((err) => {
+        throw err;
+    });
