13d380a949ce6220f8bd5441843e058043b0dacf
Stan <nedbailov375426@gmail.com>
2017-12-24 16:17:44 -0500

    Add settings to deploy

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 set -eu;
 
-URL="$(node_modules/.bin/now deploy --token=$ZEIT_TOKEN)";
+URL="$(node_modules/.bin/now deploy --team=venikman --token=$ZEIT_TOKEN)";
 node_modules/.bin/await-url "$URL" --tries=200;
-node_modules/.bin/now alias set "$URL" nedbailov.com --token=$ZEIT_TOKEN;
+node_modules/.bin/now alias set "$URL" nedbailov.com --team=venikman --token=$ZEIT_TOKEN;
 # Attempt to remove old deployment, see https://github.com/zeit/now-cli/issues/691
-node_modules/.bin/now remove noirdoor --yes --safe --token=$ZEIT_TOKEN || true;
+node_modules/.bin/now remove noirdoor --yes --safe --team=venikman --token=$ZEIT_TOKEN || true;
