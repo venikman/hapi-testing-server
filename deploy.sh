9034867e6b859e56b052e7f212d6efe55400ce40
Stan <nedbailov375426@gmail.com>
2017-12-26 19:37:37 -0500

    Fix deploy error

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 set -eu;
-npm run build;
+npm run rollup-build;
 URL="$(node_modules/.bin/now deploy --token=$ZEIT_TOKEN)";
 node_modules/.bin/await-url "$URL" --tries=200;
 node_modules/.bin/now alias set "$URL" nedbailov.com --token=$ZEIT_TOKEN;
 # Attempt to remove old deployment, see https://github.com/zeit/now-cli/issues/691
-node_modules/.bin/now remove noirdoor --yes --safe --token=$ZEIT_TOKEN || true;
+node_modules/.bin/now remove nedbailov --yes --safe --token=$ZEIT_TOKEN || true;
